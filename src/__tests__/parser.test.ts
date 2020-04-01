78eef187cdfd8ae8bb7da5dbf3b5989e7c117d80
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2020-04-01 16:19:06 +0800

    initial boilerplate
    Signed-off-by: Dmitrii Gaidarji <dmitriigaidarji@gmail.com>

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,5 +1,41 @@
-import { parseURLParams } from '../index';
+import {parseHash} from '../index';
 
-test('URL Parser', () => {
-  expect(parseURLParams('test')).toBe('Hello test');
+test('parse hash simple', () => {
+  const urlStr = '#filters={%22TS%22:[6],%22CM%22:[0,1],%22D%22:0,%22Z%22:2,%22Q%22:%22%22,%22S%22:[0],%22P%22:%22GC52%22,%22AO%22:1,%22U%22:0}';
+
+  expect(parseHash(urlStr)).toEqual({
+      filters: {
+        TS: [6],
+        CM: [0, 1],
+        D: 0,
+        Z: 2,
+        Q: '',
+        S: [0],
+        P: 'GC52',
+        AO: 1,
+        U: 0
+      }
+    }
+  );
+});
+
+test('parse hash multiple', () => {
+  const urlStr = '#filters={%22TS%22:[6],%22CM%22:[0,1],%22D%22:0,%22Z%22:2,%22Q%22:%22%22,%22S%22:[0],%22P%22:%22GC52%22,%22AO%22:1,%22U%22:0}'
+    + '&path="QC"';
+
+  expect(parseHash(urlStr)).toEqual({
+      filters: {
+        TS: [6],
+        CM: [0, 1],
+        D: 0,
+        Z: 2,
+        Q: '',
+        S: [0],
+        P: 'GC52',
+        AO: 1,
+        U: 0
+      },
+      path: 'QC'
+    }
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-export function parseURLParams(str: string) {
-  return `Hello ${str}`;
+function parsePart(key: string, value: string, result: IReturnType) {
+  const decoded = decodeURIComponent(value);
+  try {
+    result[key] = JSON.parse(decoded)
+  } catch (e) {
+    result[key] = decoded
+  }
+}
+
+interface IReturnType {
+  [key: string]: string | Array<string | number | object>
+}
+
+export function parseURLParams(inputStr: string) {
+  console.log('input', inputStr);
+  const result: IReturnType = {};
+  const strLength = inputStr.length;
+  if (strLength === 0) {
+    return result
+  }
+  try {
+    return JSON.parse(inputStr)
+  } catch (e) {
+
+  }
+  const eqSignIndex = inputStr.indexOf('=');
+  if (eqSignIndex !== -1) {
+    const splitSignIndex = inputStr.indexOf('&', eqSignIndex);
+    if (splitSignIndex !== -1) {
+      parsePart(
+        inputStr.substring(0, eqSignIndex),
+        inputStr.substring(eqSignIndex + 1, splitSignIndex),
+        result
+      );
+    } else {
+      parsePart(
+        inputStr.substring(0, eqSignIndex),
+        inputStr.substring(eqSignIndex + 1, strLength),
+        result
+      );
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+export function parseHash(inputStr: string) {
+  if (inputStr.length === 0) {
+    throw new Error('Empty string was passed');
+  }
+  if (inputStr[0] !== '#') {
+    throw new Error('String is not a valid hash. Expected it to start with #');
+  }
+  return parseURLParams(
+    inputStr.substr(1, inputStr.length)
+  )
 }
